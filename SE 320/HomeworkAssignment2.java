5aa5917eaf868872feccdb53d275f35c94f2e45a
LegroJon <jonathan.legro117@gmail.com>
2024-09-25 12:23:37 -0400

    Organization

---
+++ SE 320/HomeworkAssignment2.java
@@ -0,0 +1,20 @@
+/*
+ * SE 320 - Software Construction 
+ * Author: Jonathan Legro
+ * Date: September 21, 2024
+ * 
+ * Homework: 
+ * 
+ * Description: 
+ * 
+ * References:
+ * 
+ */
+
+public class HomeworkAssignment2 {
+
+    public static void main(String[] args) {
+        // Your code goes here
+    }
+
+}
